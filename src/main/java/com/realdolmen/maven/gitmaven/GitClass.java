af7b6fd956917b52c120a81fa9e1fe786f2a4982
JasperVB3 <43533535+JasperVB3@users.noreply.github.com>
2018-09-24 11:40:31 +0200

    test nr3 van da jasper

--- src/main/java/com/realdolmen/maven/gitmaven/GitClass.java
+++ src/main/java/com/realdolmen/maven/gitmaven/GitClass.java
@@ -7,5 +7,10 @@ public class GitClass {
     public void test(){
         System.out.println("Hello Git world");
     }
-    
+    public void test2(){
+        System.out.println("Hello Git world nr.2");
+    }
+    public void test3(){
+        System.out.println("test nr3");
+    }
 }
